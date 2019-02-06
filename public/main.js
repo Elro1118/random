dc487859cf461566d73c72b545e3fea8747c2475
Elro1118 <silvana.rojas8119@gmail.com>
2019-02-06 10:46:17 -0500

    random

--- public/main.js
+++ public/main.js
@@ -4,4 +4,24 @@ const main = () => {
   }
 }
 
+const getRandomColor = () => {
+  let letters = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)]
+  }
+  return color
+  // const c = () => {
+  //   let hex = Math.floor(Math.random() * 256).toString(16)
+  //   return ('0' + String(hex)).substr(-2) // pad with zero
+  // }
+  // return '#' + c() + c() + c()
+}
+
+const changeColor = () => {
+  document.body.style.background = getRandomColor()
+}
+
 document.addEventListener('DOMContentLoaded', main)
+
+document.querySelector('.ChangeBtn').addEventListener('click', changeColor)
